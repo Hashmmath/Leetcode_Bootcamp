1d23b7ffbeb794e0f237d9cab5e3c0d340cae6cc
Hashmmath <106550130+Hashmmath@users.noreply.github.com>
2025-10-21 20:36:43 -0400

    week-3
    Implement optimal solution to check if linked list is a palindrome.

---
+++ Palindrome Linked List.py
@@ -0,0 +1,45 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # Array Solution (not feasible since space: O(N)
+        # nums = []
+
+        # while head:
+        #     nums.append(head.val)
+        #     head = head.next
+        
+        # l, r = 0, len(nums)-1
+        # while l<=r:
+        #     if nums[l] != nums[r]:
+        #         return False
+        # return True
+
+        # Optimal Solution
+        fast = head
+        slow = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        prev = None
+        while slow:
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
+
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
+    
+    #Time : O(n)
+    #Space: O(1)
